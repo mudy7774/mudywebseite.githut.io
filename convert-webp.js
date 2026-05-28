77c0f7efc5648d7a9b642ef12b1dd887f5f79096
mudy7774 <mohamad95issa@gmail.com>
2026-05-28 02:27:06 +0200

    Add files via upload

---
+++ convert-webp.js
@@ -0,0 +1,17 @@
+﻿const Jimp = require('jimp');
+
+const files = ['my1.jpeg', 'my2.jpeg', 'mudy1.jpeg'];
+
+Promise.all(
+  files.map(async (file) => {
+    const image = await Jimp.read(file);
+    const out = file.replace(/\.(jpe?g|png)$/i, '.webp');
+    await image.quality(82).writeAsync(out);
+    console.log(`created ${out}`);
+  })
+)
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
